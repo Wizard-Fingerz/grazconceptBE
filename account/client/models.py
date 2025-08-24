ecc7ae8ee6e44a3cb1b06dbfdea85ccb255af497
WizCode <adewale.oladiti28@gmail.com>
2025-08-24 03:42:20 +0100

    Enhance Client model and serializer with new fields and properties; implement autogenerated password and default user type in ClientViewSet create method.

--- account/client/models.py
+++ account/client/models.py
@@ -7,10 +7,21 @@ from definition.models import TableDropDownDefinition
 
 
 class Client(User):
-    client_type = models.ForeignKey(TableDropDownDefinition, on_delete=models.CASCADE, limit_choices_to={'type': 'client_type'}, related_name="client_type_definition")
-    service_of_interest = models.ManyToManyField(TableDropDownDefinition, limit_choices_to={'type': 'service_of_interest'}, related_name="client_service_of_interest")
-    assign_to_teams = models.ManyToManyField(TableDropDownDefinition, limit_choices_to={'type': 'service_of_interest'}, related_name="client_service_of_interest_team")
+    client_type = models.ForeignKey(TableDropDownDefinition, on_delete=models.CASCADE, limit_choices_to={'table_name': 'client_type'}, related_name="client_type_definition")
+    service_of_interest = models.ManyToManyField(TableDropDownDefinition, limit_choices_to={'table_name': 'service_of_interest'}, related_name="client_service_of_interest")
+    assign_to_teams = models.ManyToManyField(TableDropDownDefinition, limit_choices_to={'table_name': 'service_of_interest'}, related_name="client_service_of_interest_team")
     internal_note_and_reminder = models.TextField(blank = True, null = True)
+    country = models.CharField(max_length=100, blank=True, null=True)
 
 
+    @property
+    def client_type_name(self):
+        return self.client_type.term
 
+    @property
+    def service_of_interest_name(self):
+        return self.service_of_interest.term
+    
+    @property
+    def assigned_to_teams_name(self):
+        return self.assign_to_teams.term
--- account/client/serializers.py
+++ account/client/serializers.py
@@ -1,7 +1,59 @@
 from rest_framework import serializers
 from .models import Client
 
+from django.contrib.auth.hashers import make_password
+
 class ClientSerializer(serializers.ModelSerializer):
+    client_type_name = serializers.ReadOnlyField()
+    service_of_interest_name = serializers.ReadOnlyField()
+    assigned_to_teams_name = serializers.ReadOnlyField()
+
     class Meta:
         model = Client
-        fields = '__all__'
+        fields = [
+            'id',
+            'password',
+            'last_login',
+            'is_superuser',
+            'first_name',
+            'last_name',
+            'email',
+            'country',
+            'is_staff',
+            'is_active',
+            'user_type',
+            'created_by',
+            'client_type',
+            'service_of_interest',
+            'assign_to_teams',
+            'internal_note_and_reminder',
+            'client_type_name',
+            'service_of_interest_name',
+            'assigned_to_teams_name',
+        ]
+        read_only_fields = [
+            'id',
+            'last_login',
+            'is_superuser',
+            'is_staff',
+            'is_active',
+            'date_joined',
+            'created_by',
+            'client_type_name',
+            'service_of_interest_name',
+            'assigned_to_teams_name',
+        ]
+
+    def create(self, validated_data):
+        # Hash the password before creating the Client instance
+        password = validated_data.get('password')
+        if password:
+            validated_data['password'] = make_password(password)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        # Hash the password if it's being updated
+        password = validated_data.get('password')
+        if password:
+            validated_data['password'] = make_password(password)
+        return super().update(instance, validated_data)
