095c0867cd58ee0461ac0949cffd4f7a779ae7f2
WizCode <adewale.oladiti28@gmail.com>
2025-09-23 16:22:53 +0100

    Add admin interfaces for User, Client, and ClientDocuments models with import/export functionality using django-import-export. Update admin site title and header for better clarity.

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+from account.client.documents.models import ClientDocuments
+from account.client.models import Client
+
+from .models import User
+
+class UserResource(resources.ModelResource):
+    class Meta:
+        model = User
+
+class ClientResource(resources.ModelResource):
+    class Meta:
+        model = Client
+
+class ClientDocumentsResource(resources.ModelResource):
+    class Meta:
+        model = ClientDocuments
+
+@admin.register(User)
+class UserAdmin(ImportExportModelAdmin):
+    resource_class = UserResource
+
+@admin.register(Client)
+class ClientAdmin(ImportExportModelAdmin):
+    resource_class = ClientResource
+
+@admin.register(ClientDocuments)
+class ClientDocumentsAdmin(ImportExportModelAdmin):
+    resource_class = ClientDocumentsResource
+
+
+
+admin.site.site_title = "GrazConcept Admin Dashboard"
+admin.site.site_header = "Account Administration"
