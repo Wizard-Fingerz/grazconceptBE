6545c0fb7933fb30d6283261ead59c1e61e61405
WizCode <adewale.oladiti28@gmail.com>
2025-09-23 15:57:33 +0100

    Add admin interfaces for Hotel, HotelBooking, Amenity, and FlightBooking models with import/export functionality using django-import-export.

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
 # Register your models here.
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+from .hotels.models import Hotel, HotelBooking, Amenity
+from .flight.models import FlightBooking
+
+class HotelResource(resources.ModelResource):
+    class Meta:
+        model = Hotel
+
+class HotelBookingResource(resources.ModelResource):
+    class Meta:
+        model = HotelBooking
+
+class AmenityResource(resources.ModelResource):
+    class Meta:
+        model = Amenity
+
+class FlightBookingResource(resources.ModelResource):
+    class Meta:
+        model = FlightBooking
+
+@admin.register(Hotel)
+class HotelAdmin(ImportExportModelAdmin):
+    resource_class = HotelResource
+
+@admin.register(HotelBooking)
+class HotelBookingAdmin(ImportExportModelAdmin):
+    resource_class = HotelBookingResource
+
+@admin.register(Amenity)
+class AmenityAdmin(ImportExportModelAdmin):
+    resource_class = AmenityResource
+
+@admin.register(FlightBooking)
+class FlightBookingAdmin(ImportExportModelAdmin):
+    resource_class = FlightBookingResource
