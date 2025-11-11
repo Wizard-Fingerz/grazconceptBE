28ccaf8214ce0356d5dccc0783cd908b6683004c
Adewale <adewale.oladiti28@gmail.com>
2025-11-11 15:28:56 +0100

    Add country listing and filtering by country to InstitutionViewSet
    Implemented two new actions in the InstitutionViewSet: list_countries, which returns a distinct list of countries from institutions, and list_institutions_by_country, which filters institutions based on a provided country code. This enhances the API's functionality for managing institution data by allowing users to easily access and filter institutions by their respective countries.

--- app/visa/study/institutions/views.py
+++ app/visa/study/institutions/views.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework import viewsets
 
 from app.views import CustomPagination
@@ -19,10 +21,12 @@ from .serializers import (
 #     queryset = City.objects.all()
 #     serializer_class = CitySerializer
 
+
 class ProgramTypeViewSet(viewsets.ModelViewSet):
     pagination_class = CustomPagination
     queryset = ProgramType.objects.all()
     serializer_class = ProgramTypeSerializer
+
 
 class InstitutionViewSet(viewsets.ModelViewSet):
     pagination_class = CustomPagination
@@ -66,6 +70,38 @@ class InstitutionViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         return self.serializer_class
 
+    @action(detail=False, methods=['get'], url_path='countries', url_name='institution-countries')
+    def list_countries(self, request):
+        """
+        Returns a distinct list of countries from the institutions.
+        """
+        countries = self.get_queryset().values_list('country', flat=True).distinct()
+        # If you want to map country code to country name (using django_countries' CountryField):
+        from django_countries import countries as django_countries
+        code_to_name = dict(django_countries)
+        country_list = [{"code": code, "name": code_to_name.get(
+            code, code)} for code in countries]
+        return Response(country_list)
+    
+    @action(detail=False, methods=['get'], url_path='by-country', url_name='institutions-by-country')
+    def list_institutions_by_country(self, request):
+        """
+        Returns all institutions within the specified country.
+        The country code should be provided as a query parameter "country" (e.g., ?country=US).
+        """
+        country_code = request.query_params.get('country')
+        if not country_code:
+            return Response(
+                {"detail": "country parameter is required."},
+                status=400
+            )
+        queryset = self.get_queryset().filter(country=country_code)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class CourseOfStudyViewSet(viewsets.ModelViewSet):
@@ -107,7 +143,6 @@ class CourseOfStudyViewSet(viewsets.ModelViewSet):
 
         return CustomPageSize100
 
-
     def get_queryset(self):
         """
         Optionally restricts the returned CourseOfStudy objects by filtering against
@@ -139,4 +174,3 @@ class CourseOfStudyViewSet(viewsets.ModelViewSet):
             except (ValueError, TypeError):
                 pass  # Ignore invalid limit values
         return queryset
-
