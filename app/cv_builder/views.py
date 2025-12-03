d150c0cf4371e75b9cc8d1ae7d9c04f90f1d7918
Adewale <adewale.oladiti28@gmail.com>
2025-12-03 09:48:17 +0100

    Update CVProfileViewSet to assign the request user as the profile owner during creation and updates. This change ensures that the user field is automatically set, improving data integrity and ownership management in CV profiles.

--- app/cv_builder/views.py
+++ app/cv_builder/views.py
@@ -27,18 +27,22 @@ class CVProfileViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
     def create(self, request, *args, **kwargs):
-        # Allow POST to create full CV, including related objects via nested structures.
-        serializer = self.get_serializer(data=request.data)
+        # Make sure the request user is assigned as the profile owner before serializing
+        data = request.data.copy()
+        data['user'] = request.user.pk
+        serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def update(self, request, *args, **kwargs):
-        # Handles PUT/PATCH to update the profile with all its details
+        # Make sure the request user is assigned as the profile owner before serializing
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        data = request.data.copy()
+        data['user'] = request.user.pk
+        serializer = self.get_serializer(instance, data=data, partial=partial)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
         return Response(serializer.data)
